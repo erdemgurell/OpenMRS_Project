894c3437143953939c3a92570ac8dbb0daee5fe4
Nigar Ayla Ozcanan <158833043+Cognoscentes@users.noreply.github.com>
2024-03-25 14:17:28 +0300

    Reorganized code layout and added single-line comments for clarity

--- src/US_407/PatientDeletion.java
+++ src/US_407/PatientDeletion.java
@@ -10,6 +10,8 @@ import org.testng.annotations.Test;
 // Credentials:
 // Username: admin
 // Password: Admin123
+// Website: https://demo.openmrs.org/openmrs/login.htm
+
 public class PatientDeletion extends BaseDriver {
     String usernameStr = "admin";
     String passwordStr = "Admin123";
@@ -28,6 +30,9 @@ public class PatientDeletion extends BaseDriver {
 
     @Test(priority = 1)
     public void login() {
+
+        //Log in to the application homepage as an admin user.
+        //Enter the valid credentials (valid credentials are specified above.)
         ome.mySendKeys(ome.usernamePlc, usernameStr);
         ome.mySendKeys(ome.passwordPlc, passwordStr);
         ome.myClick(ome.registrationDesk);
@@ -36,6 +41,8 @@ public class PatientDeletion extends BaseDriver {
     @Test(priority = 2)
     public void patientRegistration() {
 
+        // Create a customer/patient account for the test scenario
+        // Enter the customer data for registration
         wait.until(ExpectedConditions.elementToBeClickable(ome.registerAPatient));
         ome.myClick(ome.registerAPatient);
         ome.mySendKeys(ome.givenNamePlc, nameStr);
@@ -44,13 +51,11 @@ public class PatientDeletion extends BaseDriver {
 
         Select select = new Select(ome.genderSelect);
         select.selectByVisibleText("Female");
-
         ome.myClick(ome.nextButton);
         ome.mySendKeys(ome.birthDay, birthDayStr);
 
         select = new Select(ome.birthMonthSelect);
         select.selectByVisibleText("January");
-
         ome.mySendKeys(ome.birthYear, birtYearStr);
         ome.myClick(ome.nextButton);
         ome.mySendKeys(ome.address1, addressStr);
@@ -64,7 +69,6 @@ public class PatientDeletion extends BaseDriver {
 
         select = new Select(ome.relationshipTypeSelect);
         select.selectByVisibleText("Parent");
-
         ome.mySendKeys(ome.personName, patientRelativeStr);
         ome.myClick(ome.nextButton);
         ome.myClick(ome.confirmButton);
@@ -72,17 +76,24 @@ public class PatientDeletion extends BaseDriver {
     }
     @Test(priority = 3)
     public void deleteThePatientRecord() {
+        // Under the General Actions tab, locate and click on the Delete Patient link.
         wait.until(ExpectedConditions.elementToBeClickable(ome.deletePatientButton));
         ome.myClick(ome.deletePatientButton);
+
+       // Enter a valid reason and confirm the deletion action.
         ome.mySendKeys(ome.deletionReason, "Privacy");
         ome.myClick(ome.deletionConfirmButton);
     }
     @Test(priority = 4, dependsOnMethods = {"deleteThePatientRecord"})
     public void verifyTheDeletion() {
+
+        // Enter the name or ID of the deleted patient in the search field.
         wait.until(ExpectedConditions.visibilityOf(ome.patientSearchBox));
         ome.mySendKeys(ome.patientSearchBox, patientCredentialsStr + Keys.ENTER);
+
+        // Verify the deletion process (The patient's records should no longer be accessible in the system.)
         wait.until(ExpectedConditions.elementToBeClickable(ome.noMatchingRecordMsg));
-        Assert.assertFalse(ome.noMatchingRecordMsg.getText().equals("Cosette Tholomyes"), "No matching records found");
-        logger.info( "Search result: " + ome.noMatchingRecordMsg.getText());
+        Assert.assertNotEquals(ome.noMatchingRecordMsg.getText(), "Cosette Tholomyes", "No matching records found");
+        logger.info("Search result: " + ome.noMatchingRecordMsg.getText());
     }
 }
