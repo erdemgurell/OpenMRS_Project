178d8ccca37a100e0ee89a8aef8b3a591b84a73b
Artam <erdemmmgurel@gmail.com>
2024-03-29 09:47:36 +0300

    Us_402 HotFix

--- src/US_402/PerformLogin.java
+++ src/US_402/PerformLogin.java
@@ -1,6 +1,9 @@
 package US_402;
 
 import Utility.BaseDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class PerformLogin extends BaseDriver {
@@ -8,13 +11,15 @@ public class PerformLogin extends BaseDriver {
     String passwordStr = "Admin123";
 
     @Test
-    public void login() {
+    public void performLogin() {
         // Log in to the application homepage as an admin user.
         // Enter the valid credentials (valid credentials are specified above.)
         ome.mySendKeys(ome.usernamePlc, usernameStr);
         ome.mySendKeys(ome.passwordPlc, passwordStr);
-        ome.myClick(ome.registrationDesk);
+        ome.myClick(ome.inpatientWard);
         ome.myClick(ome.loginButton);
+
+        Assert.assertTrue(ome.adminButton.getText().contains("admin"));
 
     }
 
