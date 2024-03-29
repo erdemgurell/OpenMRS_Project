aeabc7ce0264d25e906946851291079b27bb17a5
faruk-ayrcc25 <you@example.com>
2024-03-29 10:42:47 +0300

    Login Account

---
+++ src/US_404/PatientRegistration.java
@@ -0,0 +1,19 @@
+package US_404;
+
+import Utility.BaseDriver;
+import org.testng.annotations.Test;
+
+public class PatientRegistration extends BaseDriver {
+
+    String usernameStr = "admin";
+    String passwordStr = "Admin123";
+   @Test
+    public void Login(){
+        ome.mySendKeys(ome.usernamePlc, usernameStr);
+        ome.mySendKeys(ome.passwordPlc, passwordStr);
+        ome.myClick(ome.inpatientWard);
+        ome.myClick(ome.loginButton);
+    }
+
+
+}
